a8a87811b5d553ff6c06f79d4688458380fb84ef
Sergey Karaichev <karaichev@gmail.com>
2021-12-22 10:12:47 +0300

    Actions with params

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
+    case 'DEC':
+      return state - 1;
+    case 'RAND':
+      return state + action.payload;
     default:
       return state;
   }
@@ -13,7 +17,30 @@ const reducer = (state = initialState, action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => console.log(store.getState()));
+document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'INC' });
+  });
 
-store.dispatch({ type: 'INC'} );
-store.dispatch({ type: 'INC'} );
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'DEC' });
+  });
+
+document
+  .getElementById('rand')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+
+    store.dispatch({ type: 'RAND', payload });
+  });
+
+const renew = () => {
+  document
+    .getElementById('counter')
+    .innerHTML = store.getState();
+}
+
+store.subscribe(renew)
