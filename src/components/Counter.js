c3c5896d32b78178f38fdf5581c24a296947cf45
Sergey Karaichev <karaichev@gmail.com>
2021-12-22 12:14:16 +0300

    mapDispatchToProps

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions';
+import {bindActionCreators} from 'redux';
 
 const Counter = ({ value, inc, dec, rand }) => {
   return (
@@ -18,4 +20,39 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(Counter);
+// 1.
+// import { inc, dec, rand } from '../actions';
+//
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     inc:  () => dispatch(inc()),
+//     dec:  () => dispatch(dec()),
+//     rand: () => {
+//       const value = Math.floor(Math.random() * 10);
+//       dispatch(rand(value));
+//     }
+//   }
+// }
+
+// 2.
+// const mapDispatchToProps = (dispatch) => {
+//   const {inc, dec, rand} = bindActionCreators(actions, dispatch);
+//   return {
+//     inc,
+//     dec,
+//     rand: () => {
+//       const value = Math.floor(Math.random() * 10);
+//       rand(value);
+//     }
+//   }
+// }
+
+// 3.
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+// 4.
+// export default connect(mapStateToProps, actions)(Counter);
